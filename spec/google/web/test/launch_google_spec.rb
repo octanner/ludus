11692428eda18c201bb2ff4e5de84cd4c4bc6d75
Berry North <berry.north@octanner.com>
2019-02-10 22:35:44 -0700

    updated assignments and cleaned up project

--- spec/google/web/test/launch_google_spec.rb
+++ spec/google/web/test/launch_google_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 google = Google.new
 
-feature 'Search Muramasa', type: :acceptance, sauce: ENV['USESAUCE'] do
+feature 'Search Muramasa' do
   before do
     visit(octanner.utilities.url('google'))
   end
 
   scenario 'Landing_Page' do
-    google.landing_page.click_search
+    google.landing_page.verify_text('Google')
   end
 end
